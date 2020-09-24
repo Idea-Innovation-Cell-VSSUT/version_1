0b7be443429b91c095dfcdd4238e8560cd928eba
ADYA! <62339923+adyarath@users.noreply.github.com>
2020-09-24 12:58:32 +0530

    views update

---
+++ posts/article1.php
@@ -0,0 +1,296 @@
+<?php 
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Idea Innovation cell</title>
+      <link rel="shortcut icon" href="../assets/img/iclnews.png">
+  <meta property="og:image" content="../assets/img/iclnews.png">
+  
+    
+	<meta name="keywords" content="IDEA INNOVATION CELL,IIC,IDEA IC,I INNOVATION CELL,II CELL,IIC VSSUT BURLA, IIC BHISUT BURLA, IDEA INNOVATION CELL VSSUT BURLA,VSSUT BURLA IDEA INNOVATION CELL,Idea Innovation Cell,Idea IC,I Innovation Cell,II Cell,Iic Vssut Burla,Iic Bhisut Burla,Idea Innovation Cell Vssut Burla,Vssut Idea Innovation Cell">
+    
+    
+    <meta name="description" content="This is the official website of Idea Innovation Cell, VSSUT Burla. At Idea Labs , we transform strong ideas into value for bridging the gap between Ideas and Innovation. ">
+
+  <!-- Favicons -->
+  <link href="../assets/img/iclnews.png" rel="icon">
+  <link href="../assets/img/iclnews.png" rel="apple-touch-icon">
+
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+      <link href="../font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="../assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="../assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="../assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="../assets/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="../assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="../assets/vendor/aos/aos.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="../assets/css/style.css" rel="stylesheet">
+
+  <link rel="manifest" href="manifest.json">
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="apple-mobile-web-app-status-bar-style" content="#000">
+  <meta name="apple-mobile-web-app-title" content="IIC">
+  <link rel="apple-touch-icon" href="../images/icons/icon-72x72.png" sizes="72x72">
+  <link rel="apple-touch-icon" href="../images/icons/icon-96x96.png" sizes="96x96">
+  <link rel="apple-touch-icon" href="../images/icons/icon-128x128.png" sizes="128x128">
+  <link rel="apple-touch-icon" href="../images/icons/icon-144x144.png" sizes="144x144">
+  <link rel="apple-touch-icon" href="../images/icons/icon-152x152.png" sizes="152x152">
+  <link rel="apple-touch-icon" href="../images/icons/icon-192x192.png" sizes="192x192">
+  <link rel="apple-touch-icon" href="../images/icons/icon-384x384.png" sizes="384x384">
+  <link rel="apple-touch-icon" href="../images/icons/icon-512x512.png" sizes="512x512">
+  <meta name="msapplication-TileImage" content="../images/icons/icon-144x144.png">
+  <meta name="msapplication-TileColor" content="#000">
+  <meta name="theme-color" content="#000">
+</head>
+    
+
+<body>
+<style>
+           @font-face{src:url(fonts/Helvetica.ttf);font-family:hel}@font-face{src:url(fonts/LemonMilk.otf);font-family:lem}
+</style>
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top " style="background-color: black">
+    <div class="container d-flex align-items-center justify-content-between">
+
+       <!--<h1 class="logo"><a href="index.html">Gp<span>.</span></a></h1>-->
+      <!-- Uncomment below if you prefer to use an image logo -->
+       <a href="../index.html" class="logo"><img src="../assets/img/icell_white.png" alt="" class="img-fluid"></a>
+    
+      <nav class="nav-menu d-none d-lg-block">
+        <ul>
+          <li class="active"><a href="../index.html#hero">Home</a></li>
+          <li><a href="../index.html#about">About Us</a></li>
+           <li class="drop-down"><a href="">All Events</a>
+            <ul>
+              
+                                   <li><a href="../IIC-master/Pies.html">PIES</a></li>
+                  <li><a href="../IIC-master/COVID19.html">Hack For COVID 19</a></li>
+                  <li><a href="../IIC-master/ISRO%20SEMINAR.html">ISRO SEMINAR</a></li>
+                  <li><a href="../IIC-master/ISRO%20SPACE%20ON%20WHEELS.html">ISRO SPACE ON WHEELS</a></li>
+                  
+                
+            </ul>
+          <li><a href="../IIC-master/blog2.html">Activities</a></li>
+          <li><a href="../IIC-master/blog.html">Achievements</a></li>
+          <li><a href="../index.html#team">Team</a></li>
+          <li><a href="../index.html#contact">Contact</a></li>
+          
+
+        </ul>
+      </nav><!-- .nav-menu -->
+
+      <a href="https://iicworklog.typeform.com/to/bNJ8H5" class="get-started-btn scrollto">Collaborate</a>
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+    
+        <br><br>  
+          
+  
+  <main id="main">
+
+    <!-- ======= About Section ======= -->
+    
+    <section id="services" class="services">
+      <div class="container" data-aos="fade-up">
+
+        
+          <center><img src="../assets/img/BlogStars.jpg"  width="800px" height="800px" class="img-fluid"></center><br>
+<div class="section-title">
+          
+    <center> <p>The fate of  the stars</p>
+     <img src="../IIC-master/img/IMG-20200811-WA0001.jpg" height="50px" width="50px" class="img-fluid rounded-circle" style="display: inline-block">  <a  href="https://www.linkedin.com/in/smruti-rekha-24984b192"class="text-muted" style="display: inline-block">Smruti Rekha</a>
+    
+    </center>
+        </div>
+          <center><p style="text-align: left">Stars will never fail to delight and bemuse us. Following the trends of our ancestors,stargazing is still an intense emotion of today's generation. We homo sapiens are  blessed with self consciousness and use it as one aspect of pattern recognization. Our ancestors observed the motion of the sky and they could foretell the changes on earth. Due to the lack of Scientific Knowledge and Technological shortcomings during those times our ancestors fantasized things that are hard to believe in today’s world. <br><br>
+Connecting the stars and imagining them as heroes, Gods and familiar objects was a part of their daily life. But there is a particular gorgeous group of stars known as Pleiades.This group is visible from every part of the earth. They are 40 times brighter than the sun and are about 100 million years old. They are galactic clusters which are formed from gigantic clouds of gas. Depending on conditions like how massive they are, they formed dozens of thousands of dense clusters then they contract to form individual stars. They are bound together by mutual gravity forming a galactic cluster. Unlike the solar  system where the Sun dominates the gravitational force here all the stars contribute to the overall Gravity. These stars are the blue stars which are known to be the brightest. The stars are embedded in the dust cloud which glows blue from the reflected and scattered star light.<br><br>
+According to the Greek mythology these stars are believed to be the seven maidens or seven daughters of Titan Atlas. They were spied on and  followed by Orion for 7 years and as they prayed to the Jews, the Jews converted them to the 7 gorgeous stars. The Kyle people of North America long ago believed some Young women were followed by a bear when they were dancing underneath the sky as they prayed to the Rock God. The Rock heard them cry and then it converted them to the Seven Stars. It took several years until 3 brilliant scientists unlocked the secrets of the truth of the stars.<br><br> 
+In 1901 a group of women in Harvard university were classifying the types of stars on the basis of the substance they were made up of. Annie Cannon joined a group of female astronomers nicknamed "Pickering's Women." The team, which included Williamina P. S. Fleming, worked to document and empirically classify stars. Annie Jump Cannon provided our understanding of the substance of the stars and another woman working with her knew  the way to calculate the size of the universe. Spectrum of the stars were compared to the elements present on earth and to their surprise they found these stars were also made up of the same element which also existed on earth. It took decades to classify the spectral characters of the stars. There were seven types of stars and these categories had subcategories as they varied minutely.<br><br>
+In England 1923 ,women were forbidden for pursuing advanced degrees in Science. But Cecillia Payne wasn’t going to be chained down by superstitions and dogmas in the society. Her interest in astronomy began after she attended a lecture by Arthur Eddington on his 1919 expedition to the island of Principe in the Gulf of Guinea off the west coast of Africa to observe and photograph the stars near a solar eclipse as a test of Albert Einstein's general theory of relativity. She said of the lecture: 'The result was a complete transformation of my world picture.’ She shifted to America where women were already given the freedom to study stars. She examined the chemical content and physical state of the data taken by Cannon. In 1924 Henry Norris who was the dean of American astronomers said that a major contribution of the 42-45 elements on earth are known to be present on the surface of the Sun  also but Payne contradicted it and said that the stars are made up of hydrogen and Helium million times more than other elements.However, when Payne's dissertation was reviewed, astronomer Henry Norris Russell, who stood by the theories of American physicist Henry Rowland,felt sorry for Payne and then said that her laws were fundamentally incorrect four years after then her laws were proved to be correct. <br><br>
+All stars start out by burning hydrogen, the lightest atom. Fusing hydrogen atoms into Helium then to other elements called the nuclear fusion reaction, gravity wants to crush the entire mass of the star but the energy released pushes them outwards preventing the stars from collapsing. Stars are stable because they have an outward moving pressure. Smaller stars can not fuse elements heavier than Helium.<br><br>
+Two collapses occur in the Cosmos, the gases collapse to form a star and then finally when a star is on its way to its ultimate fate.
+Ever wondered the fate of the Sun?<br>
+The balance in the nuclear fusion reaction and the gravitational force will be maintained for another 4 billion years but as the sun consumes the hydrogen the core shrinks slowly as a result the atmosphere of the Sun expands converting it into a giant red ball which will be ten times brighter than the sun as it is today. The outer atmosphere of the sun will expand and as a result it will eat Mercury and Venus and possibly the earth also. Helium gets built up in the inner core.
+ The core collapses until it becomes hot enough to fuse Helium into heavier atoms. As the sun fuses the helium atoms into carbon and oxygen you don't get any energy back out. Carbon and oxygen are the end of the sun.  energy isn’t enough to support the sun and without it, gravity wins so the entire star collapses into a highly energetic core and thus will be converted into a white dwarf star in which the atoms will form the white fluorescence.  white dwarf stars will go on shining for a hundred billion years more. But different stars have  different fates.<br><br>
+Even the stars and the celestial bodies follow the law of nature,
+ "Nothing lasts forever even the stars die".
+The fate of a star is decided by its mass. <br><br>
+
+The final moments of the stars which are more than eight times our Sun’s mass shall be something like this: the outer layers fall in at a tenth of the speed of light, bounce off the rigid core, and are ejected in an intense supernova explosion. <br><br>
+
+The fate of the stars 100 times more massive than the sun: The  Carbon and Oxygen atoms end up fusing to make more massive elements like iron<br><br>
+The core of this star, containing traces of heavier elements such as iron, implodes as a black hole.<br><br> 
+
+A white dwarf in a binary system can also explode as a supernova, leaving no remnant and expelling iron and other heavier elements generated during the explosion into interstellar space.
+The gravity of the white dwarf attracts the gas of the Red giant star when it gets out of fuel. When the gas falls on the white dwarf and will trigger nuclear explosion. This small explosion happening continuously is known as a supernova..
+
+One of the most massive stars known as the Eta. Carinae has a very different fate. Its fate is a hypernova. It lies 7,500 light-years from Earth. It will go out more like an adolescent suicide bomber, blazing through its nuclear fuel in a mere couple of million years and exploding as a supernova, a blast so violent that its flash will briefly outshine the entire Milky Way. The corpse that this kind of cosmic detonation leaves behind is a black hole. After the explosion it will emit harmful radiation. Some scientists say it won't affect earth as it is 7500 light years away. Others say that the explosion will be so evil that it will end all life on earth.
+</p></center><br><br>
+
+  </span>
+
+
+      </div>
+      <br><br>
+      <?php
+
+
+
+if(isset($_SESSION['views1']))
+
+     $_SESSION['views1'] = $_SESSION['views1']+1;
+else
+    $_SESSION['views1']=1;
+    echo"<center><h3>Views : ".$_SESSION['views1']."</h3></center>";
+   
+
+?>
+<br><br>
+      <div class="powr-comments" id="cc557f73_1600140369"></div><script src="https://www.powr.io/powr.js?platform=html"></script>
+ 
+    </section><!-- End Services Section -->
+<style>
+
+</style>
+    
+      
+    
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+     
+   <footer id="footer">
+    <div class="footer-top">
+      
+      
+    
+
+    <div class="container">
+        <center>
+        <div class="footer-info">
+              
+              <div class="social-links mt-3">
+                   <img src="../idea-sponsors.jpg" width="300px " height="300px" class="img-fluid"><br><br>
+
+                <a href="https://www.facebook.com/ideaclubvssut" class="facebook"><i class="bx bxl-facebook"></i></a>
+                <a href="https://www.instagram.com/idea_labs/" class="instagram"><i class="bx bxl-instagram"></i></a>
+
+                <a href="https://www.linkedin.com/company/idea-and-innovation-cell/" class="linkedin"><i class="bx bxl-linkedin"></i></a> 
+                  
+                 <a href="https://www.youtube.com/channel/UCl1eNHmMb4IjWveBRbKU7Iw" class="youtube"><i class="bx bxl-youtube"></i></a> 
+                <a href="https://github.com/Idea-Innovation-Cell-VSSUT" class="github"><i class="bx bxl-github"></i></a>  
+              </div>
+            </div>
+            </center>
+        <!-- AddToAny BEGIN -->
+<div class="a2a_kit a2a_kit_size_32 a2a_default_style">
+  
+  <a class="a2a_button_facebook"></a>
+  <a class="a2a_button_twitter"></a>
+  <a class="a2a_button_whatsapp"></a>
+  <a class="a2a_button_telegram"></a>
+  <a class="a2a_button_google_gmail"></a>
+  <a class="a2a_button_linkedin"></a>
+  </div>
+  <script async src="https://static.addtoany.com/menu/page.js"></script>
+  <!-- AddToAny END -->
+  <style>
+    ::selection{
+      color:white;
+      background:black;
+    }
+.a2a_kit {
+  position:fixed;
+  bottom:0px;
+  left:40%;
+  background:black;
+  padding:10px 10px 10px 10px;
+  border-radius:5px;
+}
+.a2a_button_facebook,
+.a2a_button_twitter,
+.a2a_button_whatsapp,
+.a2a_button_telegram,
+.a2a_button_google_gmail,
+.a2a_button_linkedin{
+  border-radius:100%;
+  margin-left:5px;
+}
+@media only screen and (max-width: 600px) {
+  .a2a_kit {
+  position:fixed;
+  bottom:0px;
+  left:20%;;
+  
+  background:black;
+  padding:2px 2px 2px 2px;
+  border-radius:5px;
+}
+.a2a_button_facebook,
+.a2a_button_twitter,
+.a2a_button_whatsapp,
+.a2a_button_telegram,
+.a2a_button_google_gmail,
+.a2a_button_linkedin{
+  border-radius:100%;
+  margin-left:2px;
+font-size:0.01em;
+}
+}
+  </style>
+      <div class="copyright">
+        &copy; Copyright <strong><span>IIC</span></strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        <strong>Team of Developers</strong><br><br>
+         <a href=https://github.com/rajdas2001><img src="../developers/RD.png"  width="40px" height="40px"class="img-fluid"></a>
+          <a href=https://github.com/prasenjitghose36><img src="../developers/PG.jfif"  width="40px" height="40px"class="img-fluid" ></a>
+          <a href=https://github.com/sAURYAMAN777><img src="../developers/SN.jpg"  width="40px" height="40px"class="img-fluid"></a>
+          <a href="https://github.com/patelaryan7751"><img src="../developers/AP.jpg"  width="40px" height="40px"class="img-fluid"></a>
+          <a href="https://github.com/adyarath"><img src="../developers/AR.jpeg"  width="40px" height="40px"class="img-fluid"></a>
+          
+      </div>
+    </div>
+        </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top"><i class="ri-arrow-up-line"></i></a>
+  <div id="preloader">
+      </div>
+
+  <!-- Vendor JS Files -->
+  <script src="../assets/vendor/jquery/jquery.min.js"></script>
+  <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="../assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="../assets/vendor/php-email-form/validate.js"></script>
+  <script src="../assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="../assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="../assets/vendor/venobox/venobox.min.js"></script>
+  <script src="../assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+  <script src="../assets/vendor/counterup/counterup.min.js"></script>
+  <script src="../assets/vendor/aos/aos.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="../assets/js/main.js"></script>
+    <script src="../app.js"></script>
+
+</body>
+
+</html>
